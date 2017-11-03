596f372bdfc2434ed031deb04bf1f95a99053b09
Tim Kovacic <kovacictim3@gmail.com>
2017-11-03 04:19:58 +0000

    added scket.io to backend and added client socket to index.html

--- template-server/src/app.js
+++ template-server/src/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-const config = require('../config/config.js')
+const socketIO = require('socket.io');
+const config = require('../config/config.js');
 
 const app = express();
 
@@ -15,5 +16,28 @@ app.use(cors());
 
 require('./routes')(app);
 
-app.listen(config.port);
+var server = require('http').Server(app).listen(config.port);
 console.log('Hello World! I am alive on ' + config.host + ':' + config.port);
+
+var SOCKET_LIST = {};
+var io = socketIO(server);
+
+io.on('connection', function (socket) {
+	socket.id = Math.random();
+	SOCKET_LIST[socket.id] = socket;
+	socket.on('disconnect', function() {
+		delete SOCKET_LIST[socket.id];
+		//Player.onDisconnect(socket);
+	});
+});
+
+setInterval(function() {
+	var pack = {
+		msg: 'hello world! Still Alive!'
+	}
+
+	for(var i in SOCKET_LIST) {
+		var socket = SOCKET_LIST[i];
+		socket.emit('update', pack);
+	}
+},10000); //1000/25
